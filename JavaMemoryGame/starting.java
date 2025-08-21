bfe972e6ccf559f392436f23f6659ea17d0ba864
Michael Williams <mwilliams@ohmboces.oneida-boces.org>
2025-08-21 07:42:41 -0400

    makes 2d array

--- JavaMemoryGame/starting.java
+++ JavaMemoryGame/starting.java
@@ -1,9 +1,13 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class starting {
-    public static int[] numberSet() {
+
+    // Creates a pairs of numbers 1-10. 1,1,2,2, etc.
+    public static Integer[] numberSet() {
         int number;
-        int [] array = new int[20];
+        Integer [] array = new Integer[20];
         for (number = 0; number<10; number++) {
             array[number] = number+1;
             array[number+10] = number+1;
@@ -12,17 +16,27 @@ public class starting {
         return array;
     }
 
-    public static void main(String[] args) {
-        int[] arr = numberSet();
+    public static Integer[] shuffler(Integer[] array) {
+        Integer[] arr = array;
+        List<Integer> numberList = Arrays.asList(arr);
+        Collections.shuffle(numberList);
+        Integer[] shuffled = numberList.toArray(new Integer[0]);
+        return shuffled;
+    }
 
-        
-        System.out.println("     1    2    3    4");
-        for (int i = 0; i < 5; i++) {
-            String[] blank = {i + ":  " + "[ ]", "[ ]","[ ]","[ ]","[ ]"};
-            for (int j = 0; j < 4; j++) {
-                System.out.print(blank[j] + "  ");
+    public static void main(String[] args) {
+        Integer[] list = shuffler(numberSet());
+        System.out.println(Arrays.toString(list));
+        Integer[][] twoD = new Integer[4][5];
+        int index = 0;
+        for (int j = 0; j < 4; j++){
+            for (int k = 0; k < 5; k++) {
+                twoD[j][k] = list[index++];
             }
-            System.out.println("\n");
+        }
+        
+        for (Integer[] row : twoD) {
+            System.out.println(Arrays.toString(row));
         }
     }
 
